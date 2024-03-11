21d9bb41209ac89a73f8677cd6bd08f4516ce18d
Course Test <course-test@cs.sfu.ca>
2024-03-10 17:00:59 -0700

    created user class/model

---
+++ src/main/java/com/group12/trek/models/User.java
@@ -0,0 +1,40 @@
+package com.group12.trek.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int uid;
+    private String username;
+    private String password;
+    private Boolean isAdmin;
+    
+    public int getUid() {
+        return uid;
+    }
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public Boolean getIsAdmin() {
+        return isAdmin;
+    }
+    public void setIsAdmin(Boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+}
