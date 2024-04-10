630da5433a9cba92f1a5fbfa748e7eca575451be
Course Test <course-test@cs.sfu.ca>
2024-04-09 18:20:11 -0700

    cleanup Place

--- src/main/java/com/group12/trek/models/Place.java
+++ src/main/java/com/group12/trek/models/Place.java
@@ -11,21 +11,27 @@ public class Place {
     private String geohash;
     private String name;
     private String description;
+    
     public String getGeohash() {
         return geohash;
     }
+
     public void setGeohash(String geohash) {
         this.geohash = geohash;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getDescription() {
         return description;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
