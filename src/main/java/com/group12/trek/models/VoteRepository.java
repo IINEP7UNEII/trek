5f67e8d9480397e7b12aab1baad0a65f2a3552d1
Course Test <course-test@cs.sfu.ca>
2024-04-09 18:23:39 -0700

    cleanup VoteRepository

--- src/main/java/com/group12/trek/models/VoteRepository.java
+++ src/main/java/com/group12/trek/models/VoteRepository.java
@@ -1,11 +1,10 @@
 package com.group12.trek.models;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-// import java.util.List;
+
 import java.util.Optional;
 
-
-public interface VoteRepository extends JpaRepository<Vote, Long>{
-  Optional<Vote> findByUserNameAndPostId(String userName, Long postId);
-  long deleteByUserNameAndPostId(String userName, Long postId);
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+    Optional<Vote> findByUserNameAndPostId(String userName, Long postId);
+    long deleteByUserNameAndPostId(String userName, Long postId);
 }
