9e4870fd62845b50ce9722b63150372f1b4d8efd
Course Test <course-test@cs.sfu.ca>
2024-04-09 18:19:33 -0700

    cleanup voteController

--- src/main/java/com/group12/trek/controllers/VoteController.java
+++ src/main/java/com/group12/trek/controllers/VoteController.java
@@ -2,28 +2,24 @@ package com.group12.trek.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-// import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.group12.trek.models.PostService;
 import com.group12.trek.models.VoteService;
-
 import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class VoteController {
-  @Autowired
-  VoteService voteService;
+    @Autowired
+    VoteService voteService;
 
-  @Autowired
-  PostService postService;
+    @Autowired
+    PostService postService;
 
-  @PostMapping("/upvote")
-  public String postMethodName(@RequestParam String placeGeohash, @RequestParam Long postId, @RequestParam String username) {
-    postService.upvotePostById(postId);
-    voteService.saveVote(username, postId);
-    return "redirect:/place?placeGeohash=" + placeGeohash;
-  }
-
+    @PostMapping("/upvote")
+    public String postMethodName(@RequestParam String placeGeohash, @RequestParam Long postId, @RequestParam String username) {
+        postService.upvotePostById(postId);
+        voteService.saveVote(username, postId);
+        return "redirect:/place?placeGeohash=" + placeGeohash;
+    }
 }
